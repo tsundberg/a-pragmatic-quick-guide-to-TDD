91ada3241b846ade832f7fdfc19375c5ebf9f7da
Thomas Sundberg <thomas@thinkcode.se>
2023-03-05 22:57:50 +0100

    Initial setup

---
+++ src/test/java/se/thinkcode/todo/MainIT.java
@@ -0,0 +1,13 @@
+package se.thinkcode.todo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MainIT {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
